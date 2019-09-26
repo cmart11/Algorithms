36f2b02a63e35b4eaae066250c4bdd780de3b1c9
Cristian Martinez <2cristianmartinez@gmail.com>
2019-09-26 14:32:09 -0400

    optimized permutations algo

--- permutations.js
+++ permutations.js
@@ -4,19 +4,19 @@ function getPermutations(array) {
     return perms
 }
 
-function permHelper(i, arr, perms){
-    if (i === arr.length - 1){
+function permHelper(i, arr, perms) {
+    if (i === arr.length - 1) {
         perms.push(arr.slice())
     } else {
-        for (let j = i; j < arr.length; j++){
+        for (let j = i; j < arr.length; j++) {
             swap(i, j, arr)
             permHelper(i + 1, arr, perms)
             swap(i, j, arr)
         }
-    } 
+    }
 }
 
-function swap(left, right, arr){
+function swap(left, right, arr) {
     let temp = arr[left]
     arr[left] = arr[right]
     arr[right] = temp
