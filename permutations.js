656769ebf26ed93b7b242fdb6c8452e995acd878
Cristian Martinez <2cristianmartinez@gmail.com>
2019-10-06 15:07:43 -0400

    updates

--- permutations.js
+++ permutations.js
@@ -9,14 +9,9 @@ function permHelper(i, arr, perms) {
         perms.push(arr.slice());
     } else {
         for (let j = i; j < arr.length; j++) {
-            // console.log('1', arr);
             swap(i, j, arr);
-            // console.log('2', arr);
             permHelper(i + 1, arr, perms);
-            // console.log('3', arr);
             swap(i, j, arr);
-            // console.log('4', arr);
-
         }
     }
 }
@@ -30,4 +25,6 @@ function swap(left, right, arr) {
 
 // console.log(getPermutations([1, 2, 3, 4]));
 console.log(getPermutations([1, 2, 3])); //0 perms so [[]]
+console.log(getPermutations(['A', 'B', 'C'])); //0 perms so [[]]
+
 
