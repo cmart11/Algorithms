cd60ec6eadfa061b7c4b68cce13656826785e49f
Cristian Martinez <2cristianmartinez@gmail.com>
2019-10-26 16:04:14 -0400

    strings: pattern searching

---
+++ strings/patternMatching.js
@@ -0,0 +1,40 @@
+const pattern1 = 'AABA'
+const string1 = 'AABAACAADAABAABA'
+
+
+/*
+    This is a naive pattern/string seaching approach
+    
+    where m: pattern length
+          n: string length
+    
+    Time: O((m*(n-m+1)))
+    
+
+
+*/
+
+function naivePatternSeach(str, pattern) {
+    const patternLength = pattern.length
+    const strLength = str.length
+
+    let result = []
+
+    for (let i = 0; i <= strLength - patternLength; i++) {
+
+        let j
+
+        for (j = 0; j < patternLength; j++) {
+            if (str.charAt(i + j) !== pattern[j]) {
+                break
+            }
+        }
+
+        if (j === patternLength) {
+            result.push(str.slice(i, i + patternLength))
+        }
+    }
+    return result
+}
+
+console.log(naivePatternSeach(string1, pattern1));
